984b3b5257cef665ba47a6b94a284f25f3c956a5
Ruvim Kirylka <ruvimkirylka@ruvims-mbp.lan>
2020-02-22 21:52:36 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function regexChecker() {
+    let firstName = document.getElementById('fName').value;
+    let lastName = document.getElementById('lName').value;
+    let firstnRegex = /^[a-zA-Z]/;
+
+    if (firstName.match && firstName.match){
+    alert ("Yay! Your inputs were all correct!");
+    console.log(true);
+    }
+    else{
+        alert ("Yay! Your inputs were all correct!");
+        console.log(false)
+    }
+    
+     
+
+   
+    
+    
+}
